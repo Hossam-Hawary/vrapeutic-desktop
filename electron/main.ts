9da42685ca3d4824a908a02863aa72470ad877f6
Hossam Eldeen Hawary <hossameldeen.hawary@gmail.com>
2019-11-29 22:07:46 +0200

    Fix duplicated import for electron app in main js

--- electron/main.ts
+++ electron/main.ts
@@ -1,4 +1,4 @@
-import { app, BrowserWindow, ipcMain, FeedURLOptions } from 'electron';
+import { app, BrowserWindow, ipcMain, autoUpdater } from 'electron';
 import * as path from 'path';
 import * as url from 'url';
 import * as fs from 'fs';
@@ -6,7 +6,6 @@ import { shell } from 'electron';
 import * as internalIp from 'internal-ip';
 import * as adb from 'adbkit';
 import * as capcon from 'capture-console';
-const { app, autoUpdater } = require('electron');
 const server = require('./server');
 
 const client = adb.createClient();
